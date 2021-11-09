8519e7feed9e98ea52ca07356b81c96632d42127
张微 <zhangwei@antiy.cn>
2021-11-09 15:15:16 +0800

    feat: add commitlin prettier eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-undef': 'off',
+  },
+  ignorePatterns: ['./node_modules/*', './lib/*'],
+};
